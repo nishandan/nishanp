a971f960e2e84b9c1e7a98b9ff272c322f0a39a8
Nishandan <nishanuom@gmail.com>
2018-08-15 18:36:11 +0800

    push

--- NishanTestonPhoton/src/org/nishan/pavi/time/TimeIntervalOverlapValidation.java
+++ NishanTestonPhoton/src/org/nishan/pavi/time/TimeIntervalOverlapValidation.java
@@ -22,6 +22,8 @@ public class TimeIntervalOverlapValidation {
 		System.out.println("end2 :: " + end2);
 		
 		System.out.println("isOverLaped :: " +obIntervalOverlapValidation.isOverLaped(start1, end1, start2, end2));
+		
+		System.out.println("DONE");
 
 	}
 	
